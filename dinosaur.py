837c83c38c70e42cee398c0b3d297b145d9c848e
Your Name <charles.luas.young@gmail.com>
2021-10-13 16:05:19 -0600

    Starting setting the objects in Dinosaur class so that it can be called in herd

--- dinosaur.py
+++ dinosaur.py
@@ -4,11 +4,11 @@ class Dinosaur:
         self.health = 100
         self.attack_power = 0
     
-    def set_name (self):
-        self.name = input("What is the dinosaur name?: ")
-        print("The dinosaur name is ",self.name)
+    def set_name (self, name):
+        self.name = name
+        return self.name
 
-    def set_attack_power (self):
+    def set_attack_power (self, power):
         self.attack_power = int(input(f"What is the attack power of {self.name}?"))
         print(f"The attack power of {self.name} is {self.attack_power}")
 
